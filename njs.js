a78b02d898611c455ec4bf486537cd7b1f8f2e94
K—On! <122665703+SSL-Kazu@users.noreply.github.com>
2023-07-07 09:06:10 +0800

    Update njs.js

--- njs.js
+++ njs.js
@@ -1,25 +1,21 @@
 $(document).ready(function(){
-	$('form').submit(function(event){
-		event.preventDefault();
-		var prompt=$('#prompt').val();
-		$.ajax({
-			url:'/get_answer',
-			method:'POST',
-			data:{
-				prompt:prompt
-			},
-			dataType:'json',
-			success:function(response){
-				$('#answer').text(response.answer);
-				$('#images').empty();
-				response.images.forEach(function(imageUrl){
-					$('#images').append('<img src="'+imageUrl+'">');
-				}
-				);
-			}
-		}
-		);
-	}
-	);
-}
-);
+    $('form').submit(function(event){
+        event.preventDefault();
+        var prompt = $('#prompt').val();
+        $.ajax({
+            url: '/get_answer',
+            method: 'POST',
+            data: {
+                prompt: prompt
+            },
+            dataType: 'json',
+            success: function(response){
+                $('#answer').text(response.answer);
+                $('#images').empty();
+                response.images.forEach(function(imageUrl){
+                    $('#images').append('<img src="'+imageUrl+'">');
+                });
+            }
+        });
+    });
+});
