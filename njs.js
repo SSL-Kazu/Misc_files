ea074ae5c94ce7abac363966a9d92009660d9063
K—On! <122665703+SSL-Kazu@users.noreply.github.com>
2023-07-07 09:04:44 +0800

    Add files via upload

---
+++ njs.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	$('form').submit(function(event){
+		event.preventDefault();
+		var prompt=$('#prompt').val();
+		$.ajax({
+			url:'/get_answer',
+			method:'POST',
+			data:{
+				prompt:prompt
+			},
+			dataType:'json',
+			success:function(response){
+				$('#answer').text(response.answer);
+				$('#images').empty();
+				response.images.forEach(function(imageUrl){
+					$('#images').append('<img src="'+imageUrl+'">');
+				}
+				);
+			}
+		}
+		);
+	}
+	);
+}
+);
